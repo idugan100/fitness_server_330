ffe50a3d48095cc74d230857ce4d5bb06631feee
idugan100 <isaacd4444@gmail.com>
2024-04-07 15:40:44 -0400

    setup database structure

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+-- users table structure
+CREATE TABLE Users(
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`userName` VARCHAR(255) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`isAdmin` BOOL DEFAULT false
+);
+-- notifications table structure
+CREATE TABLE Notification(
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`userID` INTEGER NOT NULL,
+	`message` VARCHAR(510),
+	`isRead` Bool DEFAULT false,
+	`created_at` DATE default CURRENT_DATE
+);
+
+-- activities table structure
+CREATE TABLE Activities (
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`userID` INTEGER NOT NULL,
+	`duration` INTEGER NOT NULL,
+	`intensity` VARCHAR(255) NOT NULL,
+	`date` DATE default CURRENT_DATE
+);
